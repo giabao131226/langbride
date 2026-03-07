e21dca7d8026078bda229e575c33d5027d06212f
giabao131226 <dobao13122006@gmail.com>
2026-03-07 22:39:03 +0700

    Done Edit Profile

--- Server/controller/client/profile.controller.js
+++ Server/controller/client/profile.controller.js
@@ -2,18 +2,22 @@
 const User = require('../../models/user.model')
 
 module.exports.editProfile = async (req,res) => {
-    const file = req.file;
+    
 
-    req.body.avatar = `/uploads/${file.filename}`;
+    const oldData = await User.find({_id: req.body._id})
+    const oldAvatar = oldData[0].avatar || ""
+    if(req.file){
+        const file = req.file;
+        req.body.avatar = `/uploads/${file.filename}`;
+    }
 
     const user =  await User.updateOne({_id: req.body._id},{
         userName: req.body.userName,
         email: req.body.email,
         phone: req.body.phone,
-        avatar: req.body.avatar
+        avatar: req.body.avatar || oldAvatar
     })
 
     const account = await User.find({_id: req.body._id})
-    
     res.status(200).json({success: true,account: account[0]});
 }
